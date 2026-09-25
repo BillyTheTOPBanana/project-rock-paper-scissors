ba2093039dcea0a71c20637c168f9b781a9b51f5
BillyTheTOPBanana <BillyTheTOPBanana@gmail.com>
2026-09-25 13:15:14 -0400

    Write playGame() and clean up comments
    Test playGame() by trying it a few times in DevTools.
    
    I realized I might want to do more work on this by, e.g., adding a GUI,
    so I decided it was worth it to clean up the comments. I moved my plans
    to the .gitignore'd dev notes.

--- rps.js
+++ rps.js
@@ -1,25 +1,5 @@
-//randomly return "rock", "paper", or "scissors"
 function getComputerChoice() {
-    /**
-     * Plan:
-     * Does JS have a built-in random integer function? If so, use it.
-     * If it just has a [0,1) RNG function, then multiply it by 3
-     * case n<1: return "rock"
-     * case 1<=n<2: return "paper"
-     * case 2<=n<3: return "scissors"
-     * 
-     * To test:
-     * Using DevTools, put a break inside the function then
-     * using the console as a REPL, run console.log(getCOmputerChoice())
-     * and make sure:
-     * - randomInt is between 0 and 3
-     * - the correct string is being returned
-     * 
-     * Results:
-     * It works! And DevTools is so easy and fast to use! I loooooove
-     * how variable values are shown to the right of their mention in the code
-     *
-     */
+    //randomly return "rock", "paper", or "scissors"
 
     const randomFloat = Math.random();
     const randomInt = randomFloat * 3;
@@ -34,94 +14,65 @@ function getComputerChoice() {
     }
 }
 
-//return the user's choice. ASSUME VALID INPUT as per TOP's instructions
 function getHumanChoice() {
-    /**
-     * Plan:
-     * use prompt(). trivial, right?
-     * 
-     * Testing:
-     * Problem:
-     * prompt() is not supported in the Live Preview console, apparently.
-     * Solution:
-     * Open the HTML in Chrome and use the DevTools console there
-     */
+    //return the user's choice. ASSUME VALID INPUT as per TOP's instructions, but allow any capitalization
 
     return prompt("Enter rock, paper, or scissors").toLowerCase();
 }
 
-//evaluate and log the result; and update scores
-function playRound(computerChoice, humanChoice) {
-    /**
-     * Plan:
-     * Since there aren't that many possibilities, let's be dumb and
-     * just account for every combination of choices. We can be slightly
-     * more concise by first checking for ties.
-     * 
-     * For the log, I want to acknowledge what each player chose, with
-     * initial-capitalization
-     */
-
-    // if (computerChoice === humanChoice) {
-    //     console.log(`Tie! You both chose ${capitalizeInitialOnly(computerChoice)}!`)
-    // }
-    // else if (computerChoice === "rock") {
-    //     if (humanChoice === "paper") {
-    //         console.log("You win! Paper beats Rock!");
-    //         ++humanScore;
-    //     }
-    //     else if(humanChoice === "scissors") {
-    //         console.log("You lose! Rock beats Scissors!");            
-    //         ++computerScore;
-    //     }
-    // }
-    // else if (computerChoice === "paper") {
-    //     if (humanChoice === "scissors") {
-    //         console.log("You win! Scissors beats Paper!");
-    //         ++humanScore;
-    //     }
-    //     else if(humanChoice === "rock") {
-    //         console.log("You lose! Paper beats Rock!");            
-    //         ++computerScore;
-    //     }
-    // }
-    // else if (computerChoice === "scissors") {
-    //     if (humanChoice === "rock") {
-    //         console.log("You win! Rock beats Scissors!");
-    //         ++humanScore;
-    //     }
-    //     else if(humanChoice === "paper") {
-    //         console.log("You lose! Scissors beats Paper!");            
-    //         ++computerScore;
-    //     }
-    // }
-    // else console.assert(false);
-
-    if (computerChoice === humanChoice) {
-       console.log(`Tie! You both chose ${capitalizeInitialOnly(computerChoice)}!`)
-    }
-    else if ( (computerChoice === "rock"     && humanChoice === "scissors") ||
-              (computerChoice === "paper"    && humanChoice === "rock")     ||
-              (computerChoice === "scissors" && humanChoice === "paper") )
-    {
-        ++computerScore;
-        console.log(`You lose! ${capitalizeInitialOnly(computerChoice)} beats ${capitalizeInitialOnly(humanChoice)}!`);
-    }
-    else if ( (humanChoice === "rock"     && computerChoice === "scissors") ||
-              (humanChoice === "paper"    && computerChoice === "rock")     ||
-              (humanChoice === "scissors" && computerChoice === "paper") )
-    {
-        ++humanScore;
-        console.log(`You win! ${capitalizeInitialOnly(humanChoice)} beats ${capitalizeInitialOnly(computerChoice)}!`);
-    }
-    else console.assert(false);
-}
-
 function capitalizeInitialOnly(str) {
+    //helper function
+
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    //play 5 rounds and log the result
+    
+    function playRound(computerChoice, humanChoice) {
+        //evaluate and log the result; and update scores
 
-playRound(getComputerChoice(), getHumanChoice());
+        //the instructions said to declare playRound inside of playGame, which is weird to me, but there ya go.
+
+        if (computerChoice === humanChoice) {
+        console.log(`Tie! You both chose ${capitalizeInitialOnly(computerChoice)}!`)
+        }
+        else if ( (computerChoice === "rock"     && humanChoice === "scissors") ||
+                (computerChoice === "paper"    && humanChoice === "rock")     ||
+                (computerChoice === "scissors" && humanChoice === "paper") )
+        {
+            ++computerScore;
+            console.log(`You lose! ${capitalizeInitialOnly(computerChoice)} beats ${capitalizeInitialOnly(humanChoice)}!`);
+        }
+        else if ( (humanChoice === "rock"     && computerChoice === "scissors") ||
+                (humanChoice === "paper"    && computerChoice === "rock")     ||
+                (humanChoice === "scissors" && computerChoice === "paper") )
+        {
+            ++humanScore;
+            console.log(`You win! ${capitalizeInitialOnly(humanChoice)} beats ${capitalizeInitialOnly(computerChoice)}!`);
+        }
+        else console.assert(false);
+    }
+    
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    for (let round=1; round<=5; round++) {        
+        console.log(`Round ${round}:`);
+        playRound(getComputerChoice(), getHumanChoice());
+    }
+    
+    let messageBuffer = `Final result:\nYour score: ${humanScore}\nComputer score: ${computerScore}\nAfter 5 rounds, `;
+    if (humanScore === computerScore) {
+        messageBuffer += "it's a tie! 🤷";
+    }
+    else if (humanScore > computerScore) {
+        messageBuffer += "you won! 😁";
+    }
+    else {
+        messageBuffer += "you lost! 😢";
+    }
+    console.log(messageBuffer);
+}
+
+playGame();
