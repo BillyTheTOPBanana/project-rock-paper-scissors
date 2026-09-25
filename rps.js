97ea381069b79a85510ea521f8c31245dfc2f039
BillyTheTOPBanana <BillyTheTOPBanana@gmail.com>
2026-09-25 12:47:53 -0400

    Write and test playRound()
    Test by creating watches for the two scores and refreshing the page a
    few times. Note I left some dev comments in the JavaScript, which I now
    realize is clutter, but since this is just a quick exercise, I'm just
    going to leave it there, and I'll do things differently in the future.

--- rps.js
+++ rps.js
@@ -50,5 +50,78 @@ function getHumanChoice() {
     return prompt("Enter rock, paper, or scissors").toLowerCase();
 }
 
+//evaluate and log the result; and update scores
+function playRound(computerChoice, humanChoice) {
+    /**
+     * Plan:
+     * Since there aren't that many possibilities, let's be dumb and
+     * just account for every combination of choices. We can be slightly
+     * more concise by first checking for ties.
+     * 
+     * For the log, I want to acknowledge what each player chose, with
+     * initial-capitalization
+     */
+
+    // if (computerChoice === humanChoice) {
+    //     console.log(`Tie! You both chose ${capitalizeInitialOnly(computerChoice)}!`)
+    // }
+    // else if (computerChoice === "rock") {
+    //     if (humanChoice === "paper") {
+    //         console.log("You win! Paper beats Rock!");
+    //         ++humanScore;
+    //     }
+    //     else if(humanChoice === "scissors") {
+    //         console.log("You lose! Rock beats Scissors!");            
+    //         ++computerScore;
+    //     }
+    // }
+    // else if (computerChoice === "paper") {
+    //     if (humanChoice === "scissors") {
+    //         console.log("You win! Scissors beats Paper!");
+    //         ++humanScore;
+    //     }
+    //     else if(humanChoice === "rock") {
+    //         console.log("You lose! Paper beats Rock!");            
+    //         ++computerScore;
+    //     }
+    // }
+    // else if (computerChoice === "scissors") {
+    //     if (humanChoice === "rock") {
+    //         console.log("You win! Rock beats Scissors!");
+    //         ++humanScore;
+    //     }
+    //     else if(humanChoice === "paper") {
+    //         console.log("You lose! Scissors beats Paper!");            
+    //         ++computerScore;
+    //     }
+    // }
+    // else console.assert(false);
+
+    if (computerChoice === humanChoice) {
+       console.log(`Tie! You both chose ${capitalizeInitialOnly(computerChoice)}!`)
+    }
+    else if ( (computerChoice === "rock"     && humanChoice === "scissors") ||
+              (computerChoice === "paper"    && humanChoice === "rock")     ||
+              (computerChoice === "scissors" && humanChoice === "paper") )
+    {
+        ++computerScore;
+        console.log(`You lose! ${capitalizeInitialOnly(computerChoice)} beats ${capitalizeInitialOnly(humanChoice)}!`);
+    }
+    else if ( (humanChoice === "rock"     && computerChoice === "scissors") ||
+              (humanChoice === "paper"    && computerChoice === "rock")     ||
+              (humanChoice === "scissors" && computerChoice === "paper") )
+    {
+        ++humanScore;
+        console.log(`You win! ${capitalizeInitialOnly(humanChoice)} beats ${capitalizeInitialOnly(computerChoice)}!`);
+    }
+    else console.assert(false);
+}
+
+function capitalizeInitialOnly(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
 let humanScore = 0;
 let computerScore = 0;
+
+playRound(getComputerChoice(), getHumanChoice());
